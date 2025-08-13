4f6e51bd1d84e20b56624cbdd41af5c647fa5a09
zmj <zach@zmj.dev>
2025-08-13 06:27:46 -0400

    minify

--- zmj/roman.fsx
+++ zmj/roman.fsx
@@ -1,18 +1,3 @@
 #!/usr/bin/env -S dotnet fsi
-
-let numerals =
-    [ "M"; "CM"; "D"; "CD"; "C"; "XC"; "L"; "XL"; "X"; "IX"; "V"; "IV"; "I" ]
-
-let values = [ 1000; 900; 500; 400; 100; 90; 50; 40; 10; 9; 5; 4; 1 ]
-
-let canHasDigit n (v, d) =
-    if n - v >= 0 then Some(n - v, d) else None
-
-let digit n =
-    List.pick (canHasDigit n) (List.zip values numerals)
-
-let rec convert (n, s) =
-    if n = 0 then s else s + convert (digit n)
-
-for line in System.IO.File.ReadLines(fsi.CommandLineArgs[1]) do
-    printfn "%s" (convert (int line, ""))
+let rec c(n,s)=if n=0 then s else s+c(List.pick(fun(v,d)->if n-v>=0 then Some(n-v,d) else None)(List.zip [1000;900;500;400;100;90;50;40;10;9;5;4;1] ["M";"CM";"D";"CD";"C";"XC";"L";"XL";"X";"IX";"V";"IV";"I"]))
+for l in System.IO.File.ReadLines fsi.CommandLineArgs[1] do printfn"%s"(c(int l,""))
